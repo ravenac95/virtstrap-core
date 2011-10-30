ca25d0a808317b1519773504692dd12d7c97e840
Reuven V. Gonzales <reuven@tobetter.us>
2011-10-30 03:41:05 -0700

    Incrementing patch version. Somehow the distribute_setup didn't get sent to pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="VirtStrap",
-    version="0.2.0",
+    version="0.2.1",
     license="MIT",
     author="Reuven V. Gonzales",
     url="https://github.com/ravenac95/virtstrap",
