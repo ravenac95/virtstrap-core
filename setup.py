f44cd4171ba4e680c1cc80a45ed6e9747578c0f5
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-13 00:17:50 -0800

    Up the patch version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="VirtStrap",
-    version="0.2.2",
+    version="0.2.3",
     license="MIT",
     author="Reuven V. Gonzales",
     url="https://github.com/ravenac95/virtstrap",
