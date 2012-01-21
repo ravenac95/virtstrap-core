fcaf6dc0c516180cbdc8da5108c526f7d592e900
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-20 16:34:18 -0800

    Moved runner into a class

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,16 +1,51 @@
-from virtstrap import runner
+"""
+Runner Tests
+============
+
+Very high level tests for the virtstrap runner
+"""
+import os
+import fudge
+from cStringIO import StringIO
+from virtstrap.runner import VirtstrapRunner
 from tests.tools import *
 
-def test_run_no_args():
-    """Run the main command line utility with no args"""
-    return_code = runner.main()
-    # No args means it should return an error value
-    assert return_code == 1
+def test_initialize_runner():
+    """Test that we can initialize the VirtstrapRunner"""
+    runner = VirtstrapRunner()
 
-def test_run_help():
-    test_args = ['--help']
-    return_code = runner.main(args=test_args)
-    assert return_code == 0
+class TestVirtstrapRunner(object):
+    def setup(self):
+        self.runner = VirtstrapRunner()
 
-def test_run_init():
-    test_args = ['init', '.']
+    def teardown(self):
+        self.runner = None
+
+    @fudge.patch('sys.stderr')
+    def test_run_no_args(self, fake_sys_stderr):
+        """Run the main command line utility with no args"""
+        fake_sys_stderr.is_a_stub()
+        try:
+            return_code = self.runner.main()
+        except SystemExit, e:
+            system_exit = True
+            assert e.code == 2
+        assert system_exit == True, "Runner didn't issue a system exit"
+
+    def test_run_help(self):
+        test_args = ['--help']
+        system_exit = False
+        try:
+            self.runner.main(args=test_args)
+        except SystemExit, e:
+            system_exit = True
+            assert e.code == 0
+        assert system_exit == True, "Runner didn't issue a system exit"
+
+    def test_run_init(self):
+        test_args = ['init', '.']
+        with in_temp_directory() as temp_directory:
+            return_code = self.runner.main(args=test_args)
+            virtual_environment_path = os.path.join(temp_directory, ".vs.env")
+            assert os.path.exists(virtual_environment_path) == True
+            assert return_code == 0
--- virtstrap/runner.py
+++ virtstrap/runner.py
@@ -1,3 +1,31 @@
+from optparse import OptionParser
+
+EXIT_FAIL = 1
+EXIT_OK = 0
+
+
+class VirtstrapRunner(object):
+    """Routes commands to different commands"""
+    def __init__(self):
+        parser = OptionParser()
+        self.parser = parser
+
+    def main(self, args=None):
+        """Runs the whole command line interface"""
+        if not args:
+            args = []
+        parser = self.parser
+        cli_options, cli_args = parser.parse_args(args=args)
+        if not cli_args:
+            parser.error('You must give a command (use "vstrap help" '
+                    'to see a list of commands)')
+        command = cli_args[0].lower()
+        return EXIT_OK
+
+    def load_command(self, name):
+        """Load command from virtstrap.commands"""
+        pass
+
 def main(args=None):
     if args == ["--help"]:
         return 0
