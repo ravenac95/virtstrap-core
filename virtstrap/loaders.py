11efe80b9575793ce9cc00b546b833883c597756
Reuven Gonzales <reuven@tobetter.us>
2012-01-22 22:10:34 -0800

    Created a command loader
    Command loader will autoload any module from virtstrap.commands.*. This
    will cause those commands to register any commands to the command
    registry.

---
+++ virtstrap/loaders.py
@@ -0,0 +1,27 @@
+import os
+from virtstrap.log import logger
+
+class CommandLoader(object):
+    def load(self):
+        """Collects all commands"""
+        self._load_builtin_commands()
+
+    def _load_builtin_commands(self):
+        from virtstrap import commands
+        builtin_command_dir = os.path.abspath(os.path.dirname(commands.__file__))
+
+        import_template = '%s.%s' % (commands.__name__, '%s')
+
+        builtin_command_files = os.listdir(builtin_command_dir)
+
+        for filename in builtin_command_files:
+            if filename.endswith('.py'):
+                # Load the file. That's all that should be necessary
+                module_name = filename[:-3]
+                import_string = import_template % module_name
+                try:
+                    __import__(import_string)
+                except ImportError:
+                    logger.exception('Failed to load builtin command '
+                        'module "%s"' % filename)
+
