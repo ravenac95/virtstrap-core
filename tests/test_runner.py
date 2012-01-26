ed0992efbc079e2c22b0ce36652e385ab94cbc21
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-25 18:08:11 -0800

    Edit testing of runner to match what argparse does

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -9,6 +9,7 @@ import fudge
 from cStringIO import StringIO
 from virtstrap.runner import VirtstrapRunner
 from tests.tools import *
+from nose.tools import raises
 
 def test_initialize_runner():
     """Test that we can initialize the VirtstrapRunner"""
@@ -24,7 +25,7 @@ class TestVirtstrapRunner(object):
     @fudge.patch('sys.stderr')
     def test_run_no_args(self, fake_sys_stderr):
         """Run the main command line utility with no args"""
-        fake_sys_stderr.is_a_stub()
+        fake_sys_stderr.is_a_stub() # just want to silence stderr
         try:
             return_code = self.runner.main()
         except SystemExit, e:
@@ -36,8 +37,12 @@ class TestVirtstrapRunner(object):
         """Run the help command"""
         test_args = ['--help']
         system_exit = False
-        code = self.runner.main(args=test_args)
-        assert code == 0
+        try:
+            code = self.runner.main(args=test_args)
+        except SystemExit, e:
+            system_exit = True
+            assert e.code == 0
+        assert system_exit == True, "Runner didn't issue a system exit"
 
     def test_run_init(self):
         """Run the init command"""
