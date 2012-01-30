3b935612ed81cc5bc7a6c29e6d77230544636f80
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-30 02:01:55 -0800

    Updated profiles and processing of VirtstrapConfig
    VirtstrapConfig now uses a default profile that is set upon
    construction. The section method can no longer set the profile per
    call. The configuration also now stores the raw and processed sections.
    The processed sections are available via the processed method.

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,7 @@
 Test VirtstrapConfig
 ====================
 """
+import fudge
 from nose.tools import *
 from tests import fixture_path
 from virtstrap.config import VirtstrapConfig, ConfigurationError
@@ -56,6 +57,7 @@ profile: profile1
 list_section:
   - value5
 """
+
 def create_fake_config():
     return VirtstrapConfig.from_string(FAKE_CONFIG)
 
@@ -66,7 +68,7 @@ def test_load_config_from_string():
 
 def test_load_from_file():
     """Load configuration from a file"""
-    config = VirtstrapConfig.from_file(fixture_path("test.yml"))
+    config = VirtstrapConfig.from_file(fixture_path('test.yml'))
     assert isinstance(config, VirtstrapConfig)
 
 @raises(ConfigurationError)
@@ -80,62 +82,82 @@ class TestVirtstrapConfig(object):
 
     def section_data_for_sections(self, section, profiles, expected_data):
         """Test section data compilation"""
-        section_data = self.config.section(section, profiles=profiles)
+        self.config.set_profiles(profiles)
+        section_data = self.config.section(section)
         assert section_data == expected_data
 
     def test_collect_multiple_list_sections(self):
         """Generates various tests for list-based sections"""
         profiles = [
             [],
-            ["profile1"],
-            ["profile2"],
-            ["profile1", "profile2"],
+            ['profile1'],
+            ['profile2'],
+            ['profile1', 'profile2'],
         ]
         expected_values = [
-            ["value1", "value2", "value3"],
-            ["value1", "value2", "value3", "value4"],
-            ["value1", "value2", "value3", "value5"],
-            ["value1", "value2", "value3", "value4", "value5"],
+            ['value1', 'value2', 'value3'],
+            ['value1', 'value2', 'value3', 'value4'],
+            ['value1', 'value2', 'value3', 'value5'],
+            ['value1', 'value2', 'value3', 'value4', 'value5'],
         ]
         tests = zip(profiles, expected_values)
         for profiles, expected in tests:
-            yield (self.section_data_for_sections, "list_section", profiles,
+            yield (self.section_data_for_sections, 'list_section', profiles,
                 expected)
 
     def test_collect_multiple_kv_sections(self):
         """Generates various tests for key value based sections"""
         profiles = [
             [],
-            ["profile1"],
-            ["profile2"],
-            ["profile1", "profile2"],
+            ['profile1'],
+            ['profile2'],
+            ['profile1', 'profile2'],
         ]
         expected_values = [
-            {"key1": "value1"},
-            {"key1": "value1", "key2": "value2"},
-            {"key1": "override1"},
-            {"key1": "override1", "key2": "value2"},
+            {'key1': 'value1'},
+            {'key1': 'value1', 'key2': 'value2'},
+            {'key1': 'override1'},
+            {'key1': 'override1', 'key2': 'value2'},
         ]
         tests = zip(profiles, expected_values)
         for profiles, expected in tests:
-            yield (self.section_data_for_sections, "kv_section", profiles,
+            yield (self.section_data_for_sections, 'kv_section', profiles,
                 expected)
 
     def test_collect_multiple_data_sections(self):
         """Generates various tests for data based sections"""
         profiles = [
             [],
-            ["profile1"],
-            ["profile2"],
-            ["profile1", "profile2"],
+            ['profile1'],
+            ['profile2'],
+            ['profile1', 'profile2'],
         ]
         expected_values = [
-            "value1",
-            "value2",
-            "value3",
-            "value3",
+            'value1',
+            'value2',
+            'value3',
+            'value3',
         ]
         tests = zip(profiles, expected_values)
         for profiles, expected in tests:
             yield (self.section_data_for_sections, "data1", profiles,
                 expected)
+
+    @fudge.test
+    def test_process_section(self):
+        process_function = fudge.Fake()
+        process_function = (process_function.expects_call()
+                .with_args(['value1', 'value2', 'value3'])
+                .returns('fake_return'))
+        self.config.process_section('list_section', process_function)
+        assert 'fake_return' == self.config.processed_section('list_section')
+
+    @fudge.test
+    def test_process_section_using_multiple_profiles(self):
+        process_function = fudge.Fake()
+        process_function = (process_function.expects_call()
+                .with_args(['value1', 'value2', 'value3', 'value4', 'value5'])
+                .returns('fake_return'))
+        self.config.set_profiles(['profile1', 'profile2'])
+        self.config.process_section('list_section', process_function)
+        assert 'fake_return' == self.config.processed_section('list_section')
--- virtstrap/config.py
+++ virtstrap/config.py
@@ -16,15 +16,22 @@ class ConfigurationCompilationError(Exception):
 
 class VirtstrapConfig(object):
     @classmethod
-    def from_string(cls, string):
-        config = cls()
+    def from_string(cls, string, profiles=None):
+        config = cls(profiles=profiles)
         config.parse(string)
         return config
 
     @classmethod
-    def from_file(cls, filename):
+    def from_file(cls, filename, profiles=None):
         string = open(filename)
-        return cls.from_string(string)
+        return cls.from_string(string, profiles=profiles)
+
+    def __init__(self, profiles=None):
+        # The default profile is ALWAYS processed no exceptions
+        self._profiles = ['default']
+        # Done here and there
+        self._profiles.extend(profiles or [])
+        self._processed_sections = {}
 
     def parse(self, string):
         raw_config_data = yaml.load_all(string)
@@ -32,22 +39,26 @@ class VirtstrapConfig(object):
         for profile in raw_config_data:
             profile_name = profile.get('profile', 'default')
             if profile_name in profile_data:
-                raise ConfigurationError('Profile "{0}" found again. Cannot '
-                        'have the same profile multiple times'
-                        .format(profile_name))
+                raise ConfigurationError('Profile "%s" found again. Cannot '
+                        'have the same profile multiple times' % profile_name)
             profile_data[profile_name] = profile
         self._raw_profile_data = profile_data
-                
-    def section(self, section_name, profiles=None):
-        # The default profile is ALWAYS processed no exceptions
-        profiles_to_compile = ['default']
-        # Add the other profiles to the process. 
-        profiles_to_compile.extend(profiles or [])
+
+    def set_profiles(self, profiles):
+        """Sets the profiles. It also resets the processed settings"""
+        self._profiles = ['default']
+        self._profiles.extend(profiles)
+        self._processed_sections = {}
+
+    def get_profile(self):
+        return self._profiles
+
+    def section(self, section_name):
         raw_profile_data = self._raw_profile_data
         # process all of the profiles requested
         compiled_data = None
-        for profile in profiles_to_compile:
-            profile_data = raw_profile_data.get(profile, None)
+        for profile in self._profiles:
+            profile_data = raw_profile_data.get(profile, {})
             section_data = profile_data.get(section_name, None)
             if section_data:
                 if compiled_data and type(compiled_data) != type(section_data):
@@ -65,6 +76,15 @@ class VirtstrapConfig(object):
                         compiled_data = section_data[:]
                     else:
                         compiled_data = section_data
+        print(self._profiles)
         return compiled_data
 
+    def process_section(self, section_name, process_function):
+        raw_section = self.section(section_name)
+        processed_section = process_function(raw_section)
+        self._processed_sections[section_name] = processed_section
+        return processed_section
 
+    def processed_section(self, section_name):
+        """Get's the processed data for a section name"""
+        return self._processed_sections.get(section_name)
