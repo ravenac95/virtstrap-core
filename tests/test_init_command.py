900677b03a65d350de71e6e90e6946c276e9767a
Reuven V. Gonzales <reuven@tobetter.us>
2012-01-31 17:45:27 -0800

    Adding some unit tests for InitializeCommand

---
+++ tests/test_init_command.py
@@ -0,0 +1,15 @@
+"""
+Initialize Command
+==================
+
+Very high level tests for the virtstrap runner
+"""
+import os
+import sys
+import fudge
+from tests.tools import *
+from nose.tools import raises
+from virtstrap.commands.init import InitializeCommand
+
+def test_initialize_command():
+    command = InitializeCommand()
