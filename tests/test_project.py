f99e4f1fd6002cb89bc393c1a58d6223bd8cbade
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-03 12:31:51 -0800

    Projects updated
    Project now successfully wraps the path generation and other project
    configuration.

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,46 +1,61 @@
 import fudge
+from fudge.inspector import arg
 from tests import fixture_path
 from tests.tools import *
-from virtstrap.project import Project, find_project_dir
+from virtstrap.project import Project, find_project_dir, ProjectNameProcessor
 from virtstrap.options import create_base_parser
 
 def test_initialize_project():
     project = Project()
 
-def test_environment_seeks_path():
+@fudge.test
+def test_project_seeks_project_path():
+    from virtstrap.config import VirtstrapConfig
     fake_project_sub_directory = fixture_path('sample_project/lev1/lev2')
     base_parser = create_base_parser()
     base_options = base_parser.parse_args(args=[])
     with in_directory(fake_project_sub_directory):
-        env = Project()
-        env.load_options(base_options)
-        assert env.project_dir == fixture_path('sample_project')
+        config = VirtstrapConfig.from_string('')
+        project = Project.load(config, base_options)
+        assert project.path() == fixture_path('sample_project')
+        assert project.name == 'sample_project'
 
 class TestProject(object):
     def setup(self):
-        self.env = Project()
-    
+        config = fudge.Fake()
+        (config.provides('process_section')
+                .with_args('project_name', arg.any())
+                .returns('projdir'))
+        base_parser = create_base_parser()
+        options = dict_to_object(dict(virtstrap_dir='/vsdir', 
+            project_dir='/projdir'))
+        self.project = Project.load(config, options)
+
+    def test_get_project_name(self):
+        """Test that the project name is found"""
+        project = self.project
+        assert project.name == 'projdir'
+
     def test_project_path(self):
-        options = dict_to_object(dict(project_dir='/projdir'))
-        env = self.env
-        env.load_options(options)
-        assert env.make_project_path('a') == '/projdir/a'
-        assert env.make_project_path('a', 'b') == '/projdir/a/b'
+        project = self.project
+        assert project.path('a') == '/projdir/a'
+        assert project.path('a', 'b') == '/projdir/a/b'
 
     def test_bin_path(self):
-        options = dict_to_object(dict(virtstrap_dir='/vsdir', 
-            project_dir='/projdir'))
-        env = self.env
-        env.load_options(options)
-        assert env.make_bin_path('test') == '/vsdir/bin/test'
+        project = self.project
+        assert project.bin_path('a') == '/vsdir/bin/a'
+        assert project.bin_path('a', 'b') == '/vsdir/bin/a/b'
 
     def test_env_path(self):
-        options = dict_to_object(dict(virtstrap_dir='/vsdir', 
-            project_dir='/projdir'))
-        env = self.env
-        env.load_options(options)
-        assert env.make_env_path('test') == '/vsdir/test'
+        project = self.project
+        assert project.env_path('a') == '/vsdir/a'
+        assert project.env_path('a', 'b') == '/vsdir/a/b'
 
+def test_process_project_name():
+    name_processor = ProjectNameProcessor('/projdir/')
+    assert 'projdir' == name_processor(None)
+    assert 'project_dir' == name_processor('project_dir')
+    
 def test_find_project_directory_from_lev2():
     fake_project_sub_directory = fixture_path('sample_project/lev1/lev2')
     with in_directory(fake_project_sub_directory):
--- virtstrap/project.py
+++ virtstrap/project.py
@@ -4,34 +4,50 @@ from virtstrap import constants
 VIRTSTRAP_DIR = constants.VIRTSTRAP_DIR
 
 class Project(object):
+    @classmethod
+    def load(cls, config, options):
+        """Creates a project and loads it's configuration immediately"""
+        project = cls()
+        project.load_settings(config, options)
+        return project
+
     def __init__(self):
         self._options = None
-
-    def load_options(self, options):
+        self._config = None
+    
+    def load_settings(self, config, options):
         self._options = options
+        self._config = config
         # Check if project directory is specified
         project_dir = getattr(options, 'project_dir', None)
         if not project_dir:
             project_dir = self._find_project_dir()
-        self.project_dir = project_dir
+        self._project_dir = project_dir
+        processor = ProjectNameProcessor(project_dir)
+        project_name = config.process_section('project_name', processor)
+        self._project_name = project_name
 
     def _find_project_dir(self):
         return find_project_dir()
 
+    @property
+    def name(self):
+        return self._project_name
+
     def set_options(self, options):
         self._options = options
 
-    def make_project_path(self, *paths):
+    def path(self, *paths):
         """Create a path relative to the project"""
-        return os.path.join(self.project_dir, *paths)
+        return os.path.join(self._project_dir, *paths)
 
-    def make_env_path(self, *paths):
+    def env_path(self, *paths):
         """Create a path relative to the virtstrap-dir"""
         return os.path.join(self._options.virtstrap_dir, *paths)
 
-    def make_bin_path(self, name):
+    def bin_path(self, *paths):
         """Create a path relative to the virtstrap-dir's bin directory"""
-        return self.make_env_path('bin', name)
+        return self.env_path('bin', *paths)
 
 def find_project_dir(current_dir=None):
     """Finds the project directory for the current directory"""
@@ -42,3 +58,11 @@ def find_project_dir(current_dir=None):
             return current_dir
     parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
     return find_project_dir(parent_dir)
+
+class ProjectNameProcessor(object):
+    def __init__(self, project_dir):
+        self._project_dir = os.path.abspath(project_dir)
+
+    def __call__(self, project_name):
+        return project_name or os.path.basename(self._project_dir)
+
