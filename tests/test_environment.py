3c69d311ae45e4596fcbd64d1aca7706d9edb97d
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-02 17:52:00 -0800

    Started work on the environment container

---
+++ tests/test_environment.py
@@ -0,0 +1,60 @@
+import fudge
+from tests import fixture_path
+from tests.tools import *
+from virtstrap.environment import Environment, find_project_dir
+from virtstrap.options import create_base_parser
+
+def test_initialize_environment():
+    env = Environment()
+
+def test_environment_seeks_path():
+    fake_project_sub_directory = fixture_path('sample_project/lev1/lev2')
+    base_parser = create_base_parser()
+    base_options = base_parser.parse_args(args=[])
+    with in_directory(fake_project_sub_directory):
+        env = Environment()
+        env.load_options(base_options)
+        assert env.project_dir == fixture_path('sample_project')
+
+class TestEnvironment(object):
+    def setup(self):
+        self.env = Environment()
+    
+    def test_project_path(self):
+        options = dict_to_object(dict(project_dir='/projdir'))
+        env = self.env
+        env.load_options(options)
+        assert env.make_project_path('a') == '/projdir/a'
+        assert env.make_project_path('a', 'b') == '/projdir/a/b'
+
+    def test_bin_path(self):
+        options = dict_to_object(dict(virtstrap_dir='/vsdir', 
+            project_dir='/projdir'))
+        env = self.env
+        env.load_options(options)
+        assert env.make_bin_path('test') == '/vsdir/bin/test'
+
+    def test_env_path(self):
+        options = dict_to_object(dict(virtstrap_dir='/vsdir', 
+            project_dir='/projdir'))
+        env = self.env
+        env.load_options(options)
+        assert env.make_env_path('test') == '/vsdir/test'
+
+def test_find_project_directory_from_lev2():
+    fake_project_sub_directory = fixture_path('sample_project/lev1/lev2')
+    with in_directory(fake_project_sub_directory):
+        project_dir = find_project_dir()
+        assert project_dir == fixture_path('sample_project')
+
+def test_find_project_directory_from_lev1():
+    fake_project_sub_directory = fixture_path('sample_project/lev1')
+    with in_directory(fake_project_sub_directory):
+        project_dir = find_project_dir()
+        assert project_dir == fixture_path('sample_project')
+
+def test_find_project_directory_from_sample_project():
+    fake_project_sub_directory = fixture_path('sample_project')
+    with in_directory(fake_project_sub_directory):
+        project_dir = find_project_dir()
+        assert project_dir == fixture_path('sample_project')
---
+++ virtstrap/environment.py
@@ -0,0 +1,44 @@
+import os
+from virtstrap import constants
+
+VIRTSTRAP_DIR = constants.VIRTSTRAP_DIR
+
+class Environment(object):
+    def __init__(self):
+        self._options = None
+
+    def load_options(self, options):
+        self._options = options
+        # Check if project directory is specified
+        project_dir = getattr(options, 'project_dir', None)
+        if not project_dir:
+            project_dir = self._find_project_dir()
+        self.project_dir = project_dir
+
+    def _find_project_dir(self):
+        return find_project_dir()
+
+    def set_options(self, options):
+        self._options = options
+
+    def make_project_path(self, *paths):
+        """Create a path relative to the project"""
+        return os.path.join(self.project_dir, *paths)
+
+    def make_env_path(self, *paths):
+        """Create a path relative to the virtstrap-dir"""
+        return os.path.join(self._options.virtstrap_dir, *paths)
+
+    def make_bin_path(self, name):
+        """Create a path relative to the virtstrap-dir's bin directory"""
+        return self.make_env_path('bin', name)
+
+def find_project_dir(current_dir=None):
+    """Finds the project directory for the current directory"""
+    current_dir = current_dir or os.path.abspath(os.curdir)
+    if VIRTSTRAP_DIR in os.listdir(current_dir):
+        vs_dir = os.path.join(current_dir, VIRTSTRAP_DIR)
+        if os.path.islink(vs_dir) or os.path.isdir(vs_dir):
+            return current_dir
+    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+    return find_project_dir(parent_dir)
