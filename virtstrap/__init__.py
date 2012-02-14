ac9fdb5585b258c6c639eb31485a8aee41da463e
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-14 12:41:59 -0800

    Added virtstrap namespace

---
+++ virtstrap/__init__.py
@@ -0,0 +1,6 @@
+# Define the virtstrap namespace.
+#
+# We might fix this later, but, aesthetically, namespace packages are 
+# beautiful.
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
