86ecebf42c86a1d107e63be39e6046c70a08635d
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-23 15:20:51 -0800

    Removed virtualenv deps in core

--- setup.py
+++ setup.py
@@ -19,18 +19,8 @@ REQUIREMENTS = [
 if sys.version_info < (2, 7):
     REQUIREMENTS.append('argparse>=1.2.1')
 
-# Install the virtstrap support files and virtualenv
-# if not in a virtstrap environment
-# or if inside a virtstrap development environment.
-# FIXME? this feels like too much of a hack. Maybe we should separate 
-# virtstrap into multiple packages (one wrapper and one core)
-if not os.environ.get('VIRTSTRAP_ENV') or \
-    os.environ.get('VIRTSTRAP_DEV_ENV'):
-    REQUIREMENTS.append('virtualenv')
-    REQUIREMENTS.append('virtstrap-support==%s' % VERSION)
-
 setup(
-    name="virtstrap",
+    name="virtstrap-core",
     version=VERSION,
     license="MIT",
     author="Reuven V. Gonzales",
