9c3bd4d79757a3b2abcb8d64b245d73323ff0eef
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-24 11:49:56 -0800

    Not using namespace packages anymore
    They just make certain things confusing more than they improve. Maybe
    we'll come back to it some day

--- virtstrap/__init__.py
+++
@@ -1,6 +0,0 @@
-# Define the virtstrap namespace.
-#
-# We might fix this later, but, aesthetically, namespace packages are 
-# beautiful.
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
